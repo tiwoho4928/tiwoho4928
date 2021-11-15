f158a68b1317a22687ef341a8bd4d1c685442abe
tiwoho4928 <94329024+tiwoho4928@users.noreply.github.com>
2021-11-14 19:27:03 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="gr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+
+	<title>ΕΠΙΛΕΞΤΕ ΤΗ ΓΛΩΣΣΑ ΣΑΣ;</title>
+</head>
+<body style="font-family: Arial;">
+	<h1 style="
+    text-align: center;
+    margin-top: 110px;
+    text-transform: uppercase;
+">ΕΠΙΛΕΞΤΕ ΤΗ ΓΛΩΣΣΑ ΣΑΣ;</h1>
+	<a href="#" style="    text-align: center;
+    display: block;
+"	><img src="flag.png" alt="" style="width: 150px;"></a>
+
+
+
+<div style="margin-top: 500px;">
+
+
+	<img src="" alt="">
+
+
+</div>
+
+</body>
+</html>
